fa9d24c00df3cdcd7d8aee46fae21a9637ab1871
afferu <afferu@gmail.com>
2017-03-27 00:27:12 +0200

    Player controller and camera improvements, Covering system prototype.

--- Assets/ThirdPersonPlayer/Scripts/PlayerController.cs
+++ Assets/ThirdPersonPlayer/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float walkSpeed = 2;
     public float runSpeed = 6;
+    public float gravity = -12;
+    public float jumpHeight = 1;
+    [Range(0, 1)]
+    public float airControlPercent;
 
     public float turnSmoothTime = 0.2f;
     float turnSmoothVelocity;
@@ -13,42 +17,113 @@ public class PlayerController : MonoBehaviour {
     public float speedSmoothTime = 0.1f;
     float speedSmoothVelocity;
     float currentSpeed;
+    float velocityY;
 
     Animator animator;
-
     Transform cameraT;
+    CharacterController controller;
 
+    bool cover = false;
+    Collider coverCollider;
 
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         //animator = GetComponent<Animator>();
         cameraT = Camera.main.transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // input
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 inputDirection = input.normalized;
-
-
-        if (inputDirection != Vector2.zero)
-        {
-            float targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
-        }
+        Vector2 inputDir = input.normalized;
 
         bool running = Input.GetKey(KeyCode.LeftShift);
 
-        float targetSpeed = (running ? runSpeed : walkSpeed) * inputDirection.magnitude;
+        Move(inputDir, running);
 
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        // animator
+        //float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+        //animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
 
-        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+    }
+    void Move(Vector2 inputDir, bool running)
+    {
+        if (cover)
+        {
+            Vector3 normal = new Vector3(0, 1, 0);
+            Vector3 tangent = Vector3.Cross(normal, coverCollider.transform.forward);
+            if (tangent.magnitude == 0)
+            {
+                tangent = Vector3.Cross(normal, Vector3.up);
+            }
+            inputDir = Input.GetAxis("Horizontal") * tangent;
+        }
+        if (inputDir != Vector2.zero)
+        {
+            //Camera rotation
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+        }
 
-        //Animation controller
-        
-        //float animationSpeedPercent = (running ? 1 : .05f) * inputDirection.magnitude;
-        //animator.SetFloat("speedPercent", animationSpeedPercent);
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
 
-	}
+        velocityY += Time.deltaTime * gravity;
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+        if (controller.isGrounded)
+        {
+            velocityY = 0;
+        }
+
+    }
+
+
+    void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+
+    float GetModifiedSmoothTime(float smoothTime)
+    {
+        if (controller.isGrounded)
+        {
+            return smoothTime;
+        }
+
+        if (airControlPercent == 0)
+        {
+            return float.MaxValue;
+        }
+        return smoothTime / airControlPercent;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("PlayerTriggerStay");
+        if ((other.gameObject.name == "Barrier"))
+        {
+            cover = true;
+            coverCollider = other;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        cover = false;
+    }
+
+
 }
--- Assets/ThirdPersonPlayer/Scripts/ThirdPersonCamera.cs
+++ Assets/ThirdPersonPlayer/Scripts/ThirdPersonCamera.cs
@@ -1,46 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class ThirdPersonCamera : MonoBehaviour {
+public class ThirdPersonCamera : MonoBehaviour
+{
 
+    public bool lockCursor;
     public float mouseSensitivity = 10;
-
     public Transform target;
-    public float distanceFromTarget = 2;
-    public Vector2 pitchMinMax = new Vector2(40, 85);
+    public float dstFromTarget = 2;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public float rotationSmoothTime = .12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
 
     float yaw;
     float pitch;
 
-    public float rotationSmoothTime = 1.2f;
-    Vector3 rotationSmoothVelocity;
-    Vector3 currentRotation;
-
-    public bool lockCursor;
-
-
-	// Use this for initialization
-	void Start () {
+    void Start()
+    {
         if (lockCursor)
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
-	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
-        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
-        pitch += Input.GetAxis("Mouse Y") * mouseSensitivity;
+    }
 
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
         pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
 
         currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
-
         transform.eulerAngles = currentRotation;
 
-        transform.position = target.position - transform.forward * distanceFromTarget;
+        transform.position = target.position - transform.forward * dstFromTarget;
 
-	}
+    }
+
 }
