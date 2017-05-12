9870b3b5bbdb612dea48bc3e4b8f765e6592e0fa
Thorin404 <zurichiiii@gmail.com>
2017-05-12 17:35:21 +0200

    cokolwiek

--- Assets/Scripts/AI Scripts/PathFinding/PathRequestManager.cs
+++ Assets/Scripts/AI Scripts/PathFinding/PathRequestManager.cs
@@ -1,4 +1,4 @@
-﻿﻿using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
@@ -9,13 +9,13 @@ public class PathRequestManager : MonoBehaviour {
 	PathRequest currentPathRequest;
 
 	static PathRequestManager instance;
-	Pathfinding pathfinding;
+	PathFinding pathfinding;
 
 	bool isProcessingPath;
 
 	void Awake() {
 		instance = this;
-		pathfinding = GetComponent<Pathfinding>();
+		pathfinding = GetComponent<PathFinding>();
 	}
 
 	public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback) {
