39a99bfd85465073cd752eff1a653bba8ee42fb1
afferu <afferu@gmail.com>
2017-06-01 01:34:01 +0200

    Importing assets, fixed some errors

--- Assets/Scripts/AI Scripts/PathFollower.cs
+++ Assets/Scripts/AI Scripts/PathFollower.cs
@@ -2,47 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathFollower : MonoBehaviour {
+public class PathFollower : MonoBehaviour
+{
 
-	public bool displayPathGizmos;
-	public Transform[] path;
-	public float speed = 2;
-	public float turnSpeed = 3.0f;
-	public float stoppingDist = 3;
-	public float reachDist = 2;
-	public int currentPoint = 0;
+    public bool displayPathGizmos;
+    public Transform[] path;
+    public float speed = 2;
+    public float turnSpeed = 3.0f;
+    public float stoppingDist = 3;
+    public float reachDist = 2;
+    public int currentPoint = 0;
 
-	void Start () {
+    void Start()
+    {
 
-	}
+    }
 
-	void Update () {
-		float dist = Vector3.Distance (path [currentPoint].position, transform.position);
-		float speedPercent = 1;
+    void Update()
+    {
+        if (path[currentPoint] != null)
+        {
+            float dist = Vector3.Distance(path[currentPoint].position, transform.position);
+            float speedPercent = 1;
 
-		if (dist <= stoppingDist) {
-			speedPercent = Mathf.Clamp01 (dist / stoppingDist);
-		}
+            if (dist <= stoppingDist)
+            {
+                speedPercent = Mathf.Clamp01(dist / stoppingDist);
+            }
 
-		Quaternion targetRotation = Quaternion.LookRotation (path[currentPoint].position - transform.position);
-		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-		transform.Translate (Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+            Quaternion targetRotation = Quaternion.LookRotation(path[currentPoint].position - transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
 
-		if (dist <= reachDist) {
-			currentPoint++;
-		}
-		if (currentPoint >= path.Length) {
-			currentPoint = 0;
-		}
-	}
+            if (dist <= reachDist)
+            {
+                currentPoint++;
+            }
+            if (currentPoint >= path.Length)
+            {
+                currentPoint = 0;
+            }
+        }
+    }
 
-	void OnDrawGizmos () {
-		if (path.Length > 0 && displayPathGizmos) {
-			for (int i = 0; i < path.Length; i++) {
-				if (path [i] != null) {
-					Gizmos.DrawSphere (path [i].position, reachDist);
-				}
-			}
-		}
-	}
+    void OnDrawGizmos()
+    {
+        if (path.Length > 0 && displayPathGizmos)
+        {
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] != null)
+                {
+                    Gizmos.DrawSphere(path[i].position, reachDist);
+                }
+            }
+        }
+    }
 }
