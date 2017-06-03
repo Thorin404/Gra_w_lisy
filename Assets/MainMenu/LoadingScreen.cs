fa292cbd5d0cbd26c42044c513af4b6b609be837
Arthur <afferu@gmail.com>
2017-06-03 12:04:12 +0200

    Adding random loading screen image script....

---
+++ Assets/MainMenu/LoadingScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour {
+
+    public Sprite[] loadingImages;
+    private Image mLoadingImage;
+
+    void OnEnable()
+    {
+        Debug.Assert(loadingImages != null);
+        mLoadingImage = GetComponent<Image>();
+
+        mLoadingImage.sprite = loadingImages[Random.Range(0, loadingImages.Length)];
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
