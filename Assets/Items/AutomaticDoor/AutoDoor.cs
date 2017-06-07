585334a64e67f0f33664523119dfbfa00fdc0248
afferu <afferu@gmail.com>
2017-06-07 23:33:54 +0200

    Adding Hammer. Padlock improvement. New Chickencoop. Adding furniture to the scene.

--- Assets/Items/AutomaticDoor/AutoDoor.cs
+++ Assets/Items/AutomaticDoor/AutoDoor.cs
@@ -1,30 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoDoor : MonoBehaviour {
+public class AutoDoor : MonoBehaviour, ILockable
+{
 
     public string playerTag;
     public string openTriggerName;
     public string closeTriggerName;
+    public float closeTime;
 
     private Animator mAnimator;
     private bool mDoorOpen = false;
+    private bool mActive;
 
-
-	// Use this for initialization
-	void Start () {
-        mAnimator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        mAnimator = GetComponentInChildren<Animator>();
+        Debug.Assert(mAnimator != null);
+        mActive = false;
+    }
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == playerTag && mAnimator.enabled)
+        if (col.gameObject.tag == playerTag && mActive && !mDoorOpen)
         {
             mDoorOpen = true;
             mAnimator.SetTrigger(openTriggerName);
@@ -33,13 +33,37 @@ public class AutoDoor : MonoBehaviour {
 
     }
 
+    private IEnumerator CloseDoor()
+    {
+        yield return new WaitForSeconds(closeTime);
+
+        mDoorOpen = false;
+        mAnimator.SetTrigger(closeTriggerName);
+        mAnimator.ResetTrigger(openTriggerName);
+    }
+
     void OnTriggerExit(Collider col)
     {
-        if (mDoorOpen && col.gameObject.tag == playerTag && mAnimator.enabled)
+        if (mDoorOpen && col.gameObject.tag == playerTag && mActive)
         {
-            mDoorOpen = false;
-            mAnimator.SetTrigger(closeTriggerName);
-            mAnimator.ResetTrigger(openTriggerName);
+            Debug.Log("Exit trigger");
+
+            StartCoroutine(CloseDoor());
         }
+    }
+
+    public void Lock()
+    {
+        mActive = false;
+    }
+
+    public void Unlock()
+    {
+        mActive = true;
+    }
+
+    public bool IsLocked()
+    {
+        return mActive;
     }
 }
