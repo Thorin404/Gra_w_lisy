238872920636b1e29cc35a65d9c83553da374c04
afferu <afferu@gmail.com>
2017-06-11 21:08:23 +0200

    Added terrain to stage 1, tutorial wip

---
+++ Assets/Tutorial/Tutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+
+    public int initialFontSize;
+    public int fontAnimationSize;
+    public float animationSpeed;
+
+    public Text textToDisplay;
+    public string[] mMessages;
+    public float[] mMessageTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Assert(mMessages != null && mMessageTime != null && textToDisplay != null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textToDisplay.fontSize = (int)Mathf.PingPong(Time.time * animationSpeed, fontAnimationSize) + initialFontSize;
+
+    }
+
+
+
+
+}
