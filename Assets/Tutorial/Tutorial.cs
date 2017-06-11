c1f52668639a8cd769a52955722914e69599946c
afferu <afferu@gmail.com>
2017-06-11 22:26:24 +0200

    Added message printing system for tutorial

--- Assets/Tutorial/Tutorial.cs
+++ Assets/Tutorial/Tutorial.cs
@@ -4,27 +4,100 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 
+public interface ITutorialAction
+{
+    bool GetState();
+    void TriggerAction();
+}
+
+
 public class Tutorial : MonoBehaviour
 {
-
     public int initialFontSize;
     public int fontAnimationSize;
     public float animationSpeed;
+    public string messageSkipButton;
 
-    public Text textToDisplay;
+    public float characterTime;
+
+    public Text messageTitle;
+    public Text messageDescription;
+
     public string[] mMessages;
+    public string[] mMessagesDescriptions;
     public float[] mMessageTime;
+
+    private string mCurrentMessageString;
+    private bool mPrintingMessage;
+    private bool mMessageEnd;
 
     // Use this for initialization
     void Start()
     {
-        Debug.Assert(mMessages != null && mMessageTime != null && textToDisplay != null);
+        Debug.Assert(mMessages != null && mMessageTime != null && messageTitle != null);
+        Debug.Assert(mMessages.Length == mMessageTime.Length);
+
+        mPrintingMessage = false;
+        mMessageEnd = false;
+        StartCoroutine(PrintMessages());
+    }
+
+    private IEnumerator PrintMessages()
+    {
+        for (int i = 0; i < mMessages.Length; ++i)
+        {
+            mMessageEnd = false;
+            messageTitle.text = mMessages[i];
+
+            StartCoroutine(PrintMessageDescription(i));
+
+            while (mPrintingMessage)
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(mMessageTime[i]);
+
+            //Wait until message is skipped
+            while (!mMessageEnd)
+            {
+                yield return null;
+            }
+        }
+
+        messageTitle.text = "";
+        messageDescription.text = "";
+
+        yield return null;
+    }
+
+    private IEnumerator PrintMessageDescription(int messageIndex)
+    {
+        mPrintingMessage = true;
+        messageDescription.fontStyle = FontStyle.Normal;
+        mCurrentMessageString = "";
+
+        for (int i=0; i< mMessagesDescriptions[messageIndex].Length; ++i)
+        {
+            mCurrentMessageString += mMessagesDescriptions[messageIndex][i];
+            messageDescription.text = mCurrentMessageString;
+            yield return new WaitForSeconds(characterTime);
+        }
+
+        messageDescription.fontStyle = FontStyle.Italic;
+        mPrintingMessage = false;
+        yield return null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        textToDisplay.fontSize = (int)Mathf.PingPong(Time.time * animationSpeed, fontAnimationSize) + initialFontSize;
+        messageTitle.fontSize = (int)Mathf.PingPong(Time.time * animationSpeed, fontAnimationSize) + initialFontSize;
+
+        if (Input.GetButtonDown(messageSkipButton))
+        {
+            mMessageEnd = true;
+        }
 
     }
 
