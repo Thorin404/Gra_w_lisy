ed4956ff95ce2c20451a7617cf4e5147e197407f
afferu <afferu@gmail.com>
2017-06-15 21:25:01 +0200

    Added animated grass, few fixes

---
+++ Assets/Assets/Models/Grass/Grass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour {
+
+    public string triggerName;
+    private Animator mAnimator;
+
+	void Start () {
+        mAnimator = GetComponent<Animator>();
+        Debug.Assert(mAnimator != null);
+    }
+	
+    void OnTriggerEnter(Collider other)
+    {
+        mAnimator.SetTrigger(triggerName);
+    }
+
+}
