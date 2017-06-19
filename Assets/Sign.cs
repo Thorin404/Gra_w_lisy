4893f03540c147ac2a911c5728cda6b57973c819
afferu <afferu@gmail.com>
2017-06-19 21:03:27 +0200

    Adding new assest and sign asset script...

---
+++ Assets/Sign.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : MonoBehaviour
+{
+
+    public MeshRenderer mSignTextRenderer;
+
+    void Start()
+    {
+        Debug.Assert(mSignTextRenderer != null);
+
+        mSignTextRenderer.enabled = false;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        mSignTextRenderer.enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        mSignTextRenderer.enabled = false;
+    }
+}
