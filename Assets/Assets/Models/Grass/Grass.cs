3b887b66b72c986f9f0b545fe5184c1c27f8884e
afferu <afferu@gmail.com>
2017-06-23 10:59:25 +0200

    Adding new animated grass with sound...

--- Assets/Assets/Models/Grass/Grass.cs
+++ Assets/Assets/Models/Grass/Grass.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class Grass : MonoBehaviour {
 
-    public string triggerName;
+    public string[] actionTriggers;
+    public float triggerReleaseTime;
+
     private Animator mAnimator;
+    private IEnumerator mAnimationStarted;
+    private AudioSource mAudioSource;
 
 	void Start () {
         mAnimator = GetComponent<Animator>();
-        Debug.Assert(mAnimator != null);
+        mAudioSource = GetComponent<AudioSource>();
+        Debug.Assert(mAnimator != null && actionTriggers != null && actionTriggers.Length != 0 && mAudioSource != null);
+        mAnimationStarted = null;
     }
 	
     void OnTriggerEnter(Collider other)
     {
-        mAnimator.SetTrigger(triggerName);
+        if(mAnimationStarted == null)
+        {
+            mAnimationStarted = StartAnimation();
+            StartCoroutine(mAnimationStarted);
+        } 
+    }
+    private IEnumerator StartAnimation()
+    {
+        //Trigger the animation
+        string trigger = actionTriggers[Random.Range(0, actionTriggers.Length)];
+        Debug.Log("Grass animation" + trigger);
+        mAnimator.SetTrigger(trigger);
+
+        //Play sound
+        mAudioSource.Play();
+
+        //Wait for specified time
+        yield return new WaitForSeconds(triggerReleaseTime);
+
+        mAnimationStarted = null;
+        yield return null;
     }
 
 }
