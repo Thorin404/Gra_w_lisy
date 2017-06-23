2ec531ac37b5161223c7f5047fbaec0dcab1b602
Thorin404 <zurichiiii@gmail.com>
2017-06-23 17:14:13 +0200

    chicken script
    dodałem skrypt dla kurczaka

--- Assets/Scripts/AI Scripts/PathFollower.cs
+++ Assets/Scripts/AI Scripts/PathFollower.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathFollower : MonoBehaviour
-{
+public class PathFollower : MonoBehaviour {
 
     public bool displayPathGizmos;
-	public bool isChicken;
 	public bool isChasing;
 
     public Transform[] path;
@@ -16,11 +14,6 @@ public class PathFollower : MonoBehaviour
 	public float chasingTurn = 4;
     public float stoppingDst = 2;
 	public float reachDst = 1;
-	public float wanderXMin;
-	public float wanderXMax;
-	public float wanderZMin;
-	public float wanderZMax;
-	public float eatingTime;
 
 	public GameController tsd;
 
@@ -28,15 +21,12 @@ public class PathFollower : MonoBehaviour
 
 	int seedsDone = 0;
 
-    void Start()
-    {
+    void Start() {
 		
     }
 
-    void Update()
-    {
-        if (path[currentPoint] != null)
-        {
+    void Update() {
+        if (path[currentPoint] != null) {
             float dist = Vector3.Distance(path[currentPoint].position, transform.position);
             float speedPercent = 1;
 
@@ -46,80 +36,27 @@ public class PathFollower : MonoBehaviour
 			if (dist <= reachDst) {
 				speedPercent = 0;
 				if (isChasing) {
-					//GetComponent<GameController> ().EndGame ("You got caught");
-					//gameObject.GetComponent<GameController>().EndGame("You got caught");
-					//GameController.EndGame ("umrzyj");
 					tsd.EndGame("You got caught");
 				}
 			}
-			Quaternion targetRotation;
-			if (isChicken) {
-				/*if (GameObject.FindGameObjectsWithTag("Seeds").Length) {
-					Vector3 chickenSeeds = GameObject.FindWithTag ("Seeds").transform.position;
-					targetRotation = Quaternion.LookRotation (chickenSeeds - transform.position);
-					transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-					if ((Vector3.Distance (chickenSeeds, transform.position)) <= 1) {
-						StartCoroutine (EatingSeeds ());
-					}
-				}else {
-					Vector3 chickenWandering = new Vector3 (Random.Range (wanderXMin, wanderXMax), 0, Random.Range (wanderZMin, wanderZMax));
-					targetRotation = Quaternion.LookRotation (chickenWandering - transform.position);
-					transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-				}*/
-				if (GameObject.FindGameObjectsWithTag ("Seeds").Length >= 1) {
-					GoingToSeeds ();
-				} else {
-					Vector3 chickenWandering = new Vector3 (Random.Range (wanderXMin, wanderXMax), 0, Random.Range (wanderZMin, wanderZMax));
-					targetRotation = Quaternion.LookRotation (chickenWandering - transform.position);
-					transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-				}
-			} else {
-				targetRotation = Quaternion.LookRotation (path [currentPoint].position - transform.position);
-				transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-			}
+			Quaternion targetRotation = Quaternion.LookRotation (path [currentPoint].position - transform.position);
+			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
             transform.Translate(Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
 
 
-            if (dist <= reachDst)
-            {
+            if (dist <= reachDst) {
                 currentPoint++;
             }
-            if (currentPoint >= path.Length)
-            {
+            if (currentPoint >= path.Length) {
                 currentPoint = 0;
             }
         }
     }
 
-	void GoingToSeeds(){
-		Quaternion targetRotation;
-		Vector3 seeds = GameObject.FindWithTag ("Seeds").transform.position;
-		targetRotation = Quaternion.LookRotation (seeds - transform.position);
-		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-		if ((Vector3.Distance (seeds, transform.position)) <= 1) {
-			StartCoroutine (EatingSeeds ());
-		}
-	}
-
-	IEnumerator EatingSeeds(){
-		speed = 0;
-		turnSpeed = 0;
-		yield return new WaitForSeconds (eatingTime);
-		GameObject tsd = GameObject.FindGameObjectWithTag ("Seeds");
-		Destroy (tsd);
-		seedsDone++;
-		speed = chasingSpeed;
-		turnSpeed = chasingTurn;
-	}
-
-    void OnDrawGizmos()
-    {
-        if (path.Length > 0 && displayPathGizmos)
-        {
-            for (int i = 0; i < path.Length; i++)
-            {
-                if (path[i] != null)
-                {
+    void OnDrawGizmos() {
+        if (path.Length > 0 && displayPathGizmos) {
+            for (int i = 0; i < path.Length; i++) {
+                if (path[i] != null) {
                     Gizmos.DrawSphere(path[i].position, reachDst);
                 }
             }
---
+++ Assets/Scripts/AI Scripts/PathFollowerChicken.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollowerChicken : MonoBehaviour {
+
+	public float speed = 2;
+	public float chasingSpeed = 3;
+	public float turnSpeed = 3;
+	public float chasingTurn = 4;
+	public float wanderXMin;
+	public float wanderXMax;
+	public float wanderZMin;
+	public float wanderZMax;
+	public float eatingTime;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		Quaternion targetRotation;
+		if (GameObject.FindGameObjectsWithTag ("Seeds").Length >= 1) {
+			GoingToSeeds ();
+		} else {
+			Vector3 chickenWandering = new Vector3 (Random.Range (wanderXMin, wanderXMax), 0, Random.Range (wanderZMin, wanderZMax));
+			targetRotation = Quaternion.LookRotation (chickenWandering - transform.position);
+			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+		}
+		transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self);
+	}
+
+	void GoingToSeeds(){
+		Quaternion targetRotation;
+		Vector3 seeds = GameObject.FindWithTag ("Seeds").transform.position;
+		targetRotation = Quaternion.LookRotation (seeds - transform.position);
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+		if ((Vector3.Distance (seeds, transform.position)) <= 1) {
+			StartCoroutine (EatingSeeds ());
+		}
+	}
+
+	IEnumerator EatingSeeds(){
+		speed = 0;
+		turnSpeed = 0;
+		yield return new WaitForSeconds (eatingTime);
+		GameObject tsd = GameObject.FindGameObjectWithTag ("Seeds");
+		Destroy (tsd);
+		speed = chasingSpeed;
+		turnSpeed = chasingTurn;
+	}
+}
