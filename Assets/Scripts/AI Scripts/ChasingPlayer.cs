8f6175cb9e06f7a98868f743b36cde7e53b6a0c0
Thorin404 <zurichiiii@gmail.com>
2017-06-26 14:30:39 +0200

    Chasing player changed
    now chasing player method is less complicated and better working

--- Assets/Scripts/AI Scripts/ChasingPlayer.cs
+++ Assets/Scripts/AI Scripts/ChasingPlayer.cs
@@ -21,20 +21,15 @@ public class ChasingPlayer : MonoBehaviour {
 
 	void Update() {
 		if (target != null) {
-			float dist = Vector3.Distance(target.position, transform.position);
-			float speedPercent = 1;
-
-			if (dist <= stoppingDst) {
-				speedPercent = dist / stoppingDst;
-			}
-			if (dist <= reachDst) {
-				speedPercent = 0;
-				tsd.EndGame("You got caught");
-
-			}
 			Quaternion targetRotation = Quaternion.LookRotation (target.position - transform.position);
 			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-			transform.Translate(Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+			transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")){
+			tsd.EndGame("You got caught");
 		}
 	}
 }
